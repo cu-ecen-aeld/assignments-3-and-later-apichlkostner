5e6b018429ff6e3e9d4d8282a00dec2ede7a8fac
Arthur Pichlkostner <a.pichlkostner@gmx.de>
2023-04-22 08:24:18 +0200

    Closing fd to exit gracefully

--- server/server.c
+++ server/server.c
@@ -20,6 +20,8 @@
 static bool server_running = true;
 static int sfd;
 
+#define RETURN_WITH_ERROR {server_running = false; ret_val = -1; break;}
+
 int server_run()
 {
     struct sockaddr_in addr;
@@ -29,6 +31,8 @@ int server_run()
     addr.sin_port = htons(PORT_NUM);
 
     sfd = socket(AF_INET, SOCK_STREAM, 0);
+    int cfd = -1;
+    int fd = -1;
 
     if (sfd == -1)
         return -1;
@@ -44,17 +48,19 @@ int server_run()
     if (listen(sfd, BACKLOG) == -1)
         return -1;
 
-    while(1) {
+    int ret_val = 0;
+
+    while(server_running) {
         char buffer[BUFFERSIZE];
         socklen_t addrlen = sizeof(struct sockaddr_storage);
         struct sockaddr_storage claddr;
         
         DEBUG_LOG("Accept");
 
-        int cfd = accept(sfd, (struct sockaddr *)&claddr, &addrlen);
+        cfd = accept(sfd, (struct sockaddr *)&claddr, &addrlen);
 
         if (cfd == -1)
-            return -1;
+            RETURN_WITH_ERROR;
 
         char host[NI_MAXHOST];
         char service[NI_MAXSERV];
@@ -73,28 +79,41 @@ int server_run()
             if (errno == EINTR)
                 continue;
             else
-                return -1;
+                RETURN_WITH_ERROR;
         }
 
-        int fd = open(LOGFILE_NAME, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        fd = open(LOGFILE_NAME, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
         if (fd == -1) {
             syslog(LOG_ERR, "Could not open logfile %s, error %s", LOGFILE_NAME, strerror(errno));
-            return -1;
+            RETURN_WITH_ERROR;
         }
         
         int num_written = write(fd, buffer, num_read);
 
         if (num_written < num_read)
-            return -1;
+            RETURN_WITH_ERROR;
 
         close(fd);
+        fd = -1;
         close(cfd);
+        cfd = -1;
+        syslog(LOG_DEBUG, "Closed connection from %s", host);
     }
 
-    close(sfd);
+    if (sfd != -1)
+        close(sfd);
+    if (fd != -1)
+        close(fd);
+    if (cfd != -1)
+        close(cfd);
 
-    return 0;
+    if ((access(LOGFILE_NAME, F_OK) == 0) && remove(LOGFILE_NAME) == -1) {
+        ERROR_LOG("Error removing logile: %s", strerror(errno));
+        ret_val = -1;
+    }
+
+    return ret_val;
 }
 
 void server_stop()
